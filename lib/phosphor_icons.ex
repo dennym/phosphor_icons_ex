691e222021b7237c621c4d96c2c8306620fa7374
Denny Mueller <contact@dennymueller.io>
2026-01-04 15:08:07 +0100

    init

---
+++ lib/phosphor_icons.ex
@@ -0,0 +1,18 @@
+defmodule PhosphorIcons do
+  @moduledoc """
+  Documentation for `PhosphorIcons`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhosphorIcons.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
